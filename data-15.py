7793cbecaf7e5dab246750ef2e0505392889850d
Dias-Prameswari <114818845+Dias-Prameswari@users.noreply.github.com>
2024-07-01 21:26:06 +0700

    Delete data-15.py

--- data-15.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-# datanya terlalu banyak untuk ditampilkan & tdk error 
-df = pd.read_csv("data/Dataset_Sentimen_Emosi.csv")
-# untuk menampilkan semua data
-# pd.set_option('display.max_rows', None)
-print(df)
