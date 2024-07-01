df0cba7317aa9b079d42c82e60f5c06ed1205975
Dias-Prameswari <114818845+Dias-Prameswari@users.noreply.github.com>
2024-07-01 21:25:00 +0700

    Delete data-10.py

--- data-10.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-# datanya terlalu banyak untuk ditampilkan
-df = pd.read_csv("data/pseudo-labelling.csv")
-# untuk menampilkan semua data
-# pd.set_option('display.max_rows', None)
-print(df)
