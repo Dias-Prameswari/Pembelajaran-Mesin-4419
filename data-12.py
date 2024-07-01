41a0a1c64e1103fb0f4822424e3755668984c9e3
Dias-Prameswari <114818845+Dias-Prameswari@users.noreply.github.com>
2024-07-01 21:25:31 +0700

    Delete data-12.py

--- data-12.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-# datanya terlalu banyak untuk ditampilkan
-df = pd.read_csv("data/diabetes.csv")
-# untuk menampilkan semua data
-# pd.set_option('display.max_rows', None)
-print(df)
