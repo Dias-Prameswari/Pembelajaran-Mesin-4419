671be7905b11d83cc90b893fc3718e06897e8a53
Dias-Prameswari <114818845+Dias-Prameswari@users.noreply.github.com>
2024-07-01 21:26:17 +0700

    Delete data-16.py

--- data-16.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-# datanya terlalu banyak untuk ditampilkan & tdk error 
-df = pd.read_csv("data/kamus_clean.csv")
-# untuk menampilkan semua data
-# pd.set_option('display.max_rows', None)
-print(df)
