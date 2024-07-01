65af339f3f5af7b6093838c00d4255cbebbb55bd
Dias-Prameswari <114818845+Dias-Prameswari@users.noreply.github.com>
2024-07-01 21:28:39 +0700

    Delete data-2.py

--- data-2.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-# datanya terlalu banyak untuk ditampilkan
-df = pd.read_csv("data/epl-goalScorer(20-21).csv")
-# untuk menampilkan semua data
-# pd.set_option('display.max_rows', None) 
-print(df)
